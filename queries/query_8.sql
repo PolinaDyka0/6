98d2979d02a46bc20fc012cecd67ab12c16580b4
PolinaDyka0 <115357542+PolinaDyka0@users.noreply.github.com>
2023-03-28 23:29:06 +0300

    Update query_8.sql

--- queries/query_8.sql
+++ queries/query_8.sql
@@ -2,7 +2,7 @@
 
 SELECT teachers.fullname, ROUND(AVG(grades.grade), 2) as avg_grade
 FROM teachers
-INNER JOIN disciplines ON teachers.id = disciplines.teacher_id
-INNER JOIN grades ON disciplines.id = grades.discipline_id
+JOIN disciplines ON teachers.id = disciplines.teacher_id
+JOIN grades ON disciplines.id = grades.discipline_id
 GROUP BY teachers.id
 HAVING teachers.fullname = 'Яків Баклан'
