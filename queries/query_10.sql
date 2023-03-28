4b187479dad6406be9b11fb95342dab4e2b0f40f
PolinaDyka0 <115357542+PolinaDyka0@users.noreply.github.com>
2023-03-28 23:27:56 +0300

    Update query_10.sql

--- queries/query_10.sql
+++ queries/query_10.sql
@@ -3,9 +3,9 @@
 
 SELECT disciplines.name 
 FROM grades 
-INNER JOIN students ON grades.student_id = students.id 
-INNER JOIN disciplines ON grades.discipline_id = disciplines.id 
-INNER JOIN teachers ON disciplines.teacher_id = teachers.id 
+JOIN students ON grades.student_id = students.id 
+JOIN disciplines ON grades.discipline_id = disciplines.id 
+JOIN teachers ON disciplines.teacher_id = teachers.id 
 WHERE students.fullname = 'Устим Височан' AND teachers.fullname = 'Амалія Власюк'
 GROUP BY disciplines.name;
 
